3b89056e82e0b431ab1d9eff213efd93570e1de5
Alberto-Luis15 <Luis_arq8@outlook.es>
2019-09-20 13:31:30 -0500

    Se la añadio un menu y el de adherir datos

--- practica5.cpp
+++ practica5.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 struct NoDo{
 	int dato;
@@ -12,78 +12,52 @@ struct NoDo{
 		}
 };
 NoDo x1(3), x2(7), x3(9), x4(12), x5(15);
+void adherir(struct NoDo*&, int);
+
 int main(int argc, char** argv){
-	struct NoDo *inicio;
-	struct NoDo *auxiliar;
-	inicio=NULL;
-	auxiliar=new NoDo(0);
+	int option, add;
+	struct NoDo *lista(0);
+	while (option!=3){
 	
-	cout<<"Nodos: \n"<<endl;
-	auxiliar->dato=10;
-	auxiliar->puntero=NULL;
-	cout<<auxiliar->dato<<endl;
-	cout<<endl;
+	cout<<"Selecciona una opcion del Menu"<<endl;
+	cout<<"1. Llenar nodos"<<endl;
+	cout<<"2. Mostrar datos de nodos"<<endl;
+	cout<<"3. Exit"<<endl;
+	cin>>option;
+	switch (option){
+		case 1:
+			cout<<"Llenar nodos: "<<endl;
+			cin>>add;
+			adherir(lista,add);
+			break;
+		case 2:
+			cout<<"Mostrando datos: "<<endl;
+		
+			break;
+		case 3:
+			cout<<"Salir del programa: "<<endl;
+		default: 
+			cout<<".:Bye:."<<endl;
+			break;
+			
+	}
+}
 	
-	inicio=auxiliar;
-	auxiliar=new NoDo(0);
-	auxiliar->dato=31;
-	auxiliar->puntero=NULL;
-	cout<<auxiliar->dato<<endl;
-	cout<<endl;
-	
-	
-	inicio->puntero=auxiliar;
-	auxiliar=new NoDo(0);
-	auxiliar->dato=11;
-	auxiliar->puntero=NULL;
-	cout<<auxiliar->dato<<endl;
-	cout<<endl;
-	
-	
-	inicio->puntero->puntero=auxiliar;
-	auxiliar=new NoDo(0);
-	auxiliar->dato=21;
-	auxiliar->puntero=NULL;
-	cout<<auxiliar->dato<<endl;
-	cout<<endl;
-	
-	
-	inicio->puntero->puntero->puntero=auxiliar;
-	auxiliar=new NoDo(0);
-	auxiliar->dato=45;
-	auxiliar->puntero=NULL;
-	cout<<auxiliar->dato<<endl;
-	cout<<endl;
-	
-
-
-	
-	cout<<"Valores:1\n"<<endl;
-	cout<<x1.dato<<endl<<endl;
-	cout<<x2.dato<<endl<<endl;
-	cout<<x3.dato<<endl<<endl;
-	cout<<x4.dato<<endl<<endl;
-	cout<<x5.dato<<endl<<endl;
-	
-	x1.puntero=&x2;
-	x2.puntero=&x3;
-	x3.puntero=&x4;
-	x4.puntero=&x5;
-	cout<<"Valores: \n"<<endl;
-	cout<<x1.dato<<endl;
-	cout<<(x1.puntero)->dato<<endl;
-	cout<<(x1.puntero)->puntero->dato<<endl;
-	cout<<(x1.puntero)->puntero->puntero->dato<<endl;
-	cout<<(x1.puntero)->puntero->puntero->puntero->dato<<endl;
-	cout<<(x1.puntero)->puntero->puntero->puntero->puntero->dato<<endl;
-	
-	cout<<"Lista dinamica: \n"<<endl;
-	cout<<inicio->dato<<endl;
-	cout<<(inicio->puntero)->dato<<endl;
-	cout<<(inicio->puntero)->puntero->dato<<endl;
-	cout<<(inicio->puntero)->puntero->puntero->dato<<endl;
-	cout<<(inicio->puntero)->puntero->puntero->puntero->dato<<endl;
 	
 	return 0;
 }
 
+void adherir(NoDo*& lista, int add)
+{
+	NoDo *nuevo=new NoDo(0);
+	nuevo->puntero=NULL;
+	nuevo->dato=add;
+	if(lista==NULL)lista=nuevo;
+	else{
+		NoDo *auxiliar=lista;
+		while(auxiliar->puntero!=NULL){
+			auxiliar=auxiliar->puntero;
+		}
+		auxiliar->puntero=nuevo;
+	}
+} 
